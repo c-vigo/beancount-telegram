dfdd5e59c72ec6f2525a6b6b127c77489e7d0a28
Carlos Vigo <carviher1990@gmail.com>
2022-09-06 09:38:05 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# README
+
+[![PyPI Latest Version](https://badge.fury.io/py/pdf-importer.svg)](https://badge.fury.io/py/pdf-importer)
+[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
+
+## Beancount Telegram
+
+## Authors
+
+* [**Carlos Vigo**](mailto:carviher1990@gmail.com?subject=[GitHub%-%beancount-telegram]) - *Initial work* - 
+[GitHub](https://github.com/c-vigo)
+
+## Contributing
+
+Please read our [contributing policy](CONTRIBUTING.md) for details on our code of
+conduct, and the process for submitting pull requests to us.
+
+## Versioning
+
+We use [Git](https://git-scm.com/) for versioning. For the versions available, see the 
+[tags on this repository](https://gitlab.ethz.ch/exotic-matter/cw-beam/pdf-importer).
+
+## License
+
+This project is licensed under the [GNU GPLv3 License](LICENSE.md)
+
+## Built With
+
+* [PyCharm Professional 2020](https://www.jetbrains.com/pycharm//) - The IDE used
---
+++ setup.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+# Author: Carlos Vigo
+# Contact: carviher1990@gmail.com
+
+from setuptools import setup
+
+from os.path import join, dirname, abspath
+from sys import path as sys_path
+sys_path.append(abspath('beancount_telegram'))
+import __project__                        # noqa: E402
+
+
+# Read the README.md file
+with open(join(dirname(__file__), 'README.md'), "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name=__project__.__package_name__,
+    version=__project__.__version__,
+    author=__project__.__short_author__,
+    author_email=__project__.__email__,
+    description=__project__.__description__,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=__project__.__url__,
+    packages=['beancount-telegram'],
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: POSIX :: Linux",
+    ],
+    license='GPLv3',
+    keywords=[
+        'Telegram'
+    ],
+    python_requires='>=3.6',
+    setup_requires=[
+        'pip>=10.0',
+        'wheel',
+        'setuptools>=30',
+    ],
+    install_requires=[
+        'telethon',
+        'python-dateutil',
+        'cryptg'
+    ],
+    include_package_data=True
+)
